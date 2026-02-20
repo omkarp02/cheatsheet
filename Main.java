4974a2b8163864e6d58e6ecca56877c4bb13b6e9
Omkar Pawar <opwebdev01@gmail.com>
2026-02-20 09:19:03 +0530

    latest change

--- Main.java
+++ Main.java
@@ -1,40 +1,51 @@
 import java.util.List;
+import java.util.Map;
 import java.util.Arrays;
+import java.util.HashMap;
 
 class Main {
     public static void main(String[] args) {
 
         // price = [2,5], special = [[3,0,5],[1,2,10]], needs = [3,2]
         Solution solution = new Solution();
-        int[] nums1 = { 1, 2, 5 };
-        int result = solution.change(5, nums1);
+        int[] nums1 = { 1,2,1,2,3 };
+        int result = solution.subarraysWithKDistinct(nums1, 2);
         System.out.println("Max Dot Product: " + result);
 
     }
 }
 
 class Solution {
-    public int change(int amount, int[] coins) {
-        int dp[][] = 
+
+    // nums = [1,2,1,2,3], k = 2
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        int a = helper(nums, k);
+        int b = helper(nums, k - 1);
+        return a - b;
     }
 
-    public int helper(int i, int target, int[] coins) {
-        if (i == 0) {
-            if (target == 0 || target == coins[i])
-                return 1;
-            if (target % coins[i] == 0) {
-                return 1;
+    // nums = [1,2,1,2,3], k = 2
+    public int helper(int[] nums, int k){
+        if(k == 0) return 0;
+ 
+        int l = 0;
+        int r = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        while(r < nums.length){
+            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
+            while(map.size() > k){
+                map.put(nums[l], map.get(nums[l]) - 1);
+                if(map.get(nums[l]) == 0) {
+                    map.remove(nums[l]);
+                };
+                l += 1;
             }
-            return 0;
+
+            count += r - l + 1;
+            r += 1;
         }
-
-        int sum = 0;
-        if (coins[i] <= target) {
-            sum += helper(i, target - coins[i], coins);
-        }
-
-        sum += helper(i - 1, target, coins);
-
-        return sum;
+        
+        return count; 
     }
-
+}
